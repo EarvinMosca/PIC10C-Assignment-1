b9673ae49a5035a565e302219952da9d92372afe
EarvinMosca <earvinpaolomosca@yahoo.com>
2017-04-20 22:26:59 -0700

    Initial Commit

---
+++ PIC10C 1/PIC10C 1/siete_y_medio.cpp
@@ -0,0 +1,33 @@
+/* *************************************
+Earvin Mosca
+April 21, 2017
+
+The program runs the game "Siete y Medio"
+and records the rounds played in
+"gamelog.txt".
+
+************************************* */
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <ctime>
+#include <cstdlib>
+#include "cards.h"
+using namespace std;
+
+// Global constants (if any)
+
+
+// Non member functions declarations (if any)
+
+
+// Non member functions implementations (if any)
+
+
+// Stub for main
+int main(){
+	/* --STATEMENTS-- */
+	return 0;
+}
